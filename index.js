dcb694503297e98c5301bbd92e367107917f0a1c
Simbiat1 <simbiatjinadu01@gmail.com>
2025-02-22 19:56:23 +0100

    implemented url extraction from html

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import winston from "winston";
 import cors from "cors";
+import cheerio from "cheerio";
 
 const { combine, timestamp, json, prettyPrint, errors } = winston.format;
 
@@ -32,6 +33,19 @@ const logger = winston.createLogger({
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Function to extract URLs from HTML content
+function extractUrlsFromHtml(html) {
+    const $ = cheerio.load(html);
+    const urls = [];
+    $('a').each((index, element) => {
+        const url = $(element).attr('href');
+        if (url) {
+            urls.push(url);
+        }
+    });
+    return urls;
+}
+
 // Function to shorten a link using Bitly API
 async function shortenLink(longLink) {
     try {
@@ -54,20 +68,22 @@ async function shortenLink(longLink) {
 app.post('/shortenURL', async (req, res) => {
      // Logs incoming request
      logger.info("Incoming request", { body: req.body });
-     
+
     const { message, settings, channel_id } = req.body; 
 
     if (!message || !settings || !channel_id) {
         return res.status(400).json({ error: 'Message, channel_id, and settings are required' });
     }
 
+    //Extract urls from html message
+    const urls = extractUrlsFromHtml(message);
 
-    // Regular expression to find URLs in the message
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const urls = message.match(urlRegex); // Finds all URLs in the message
+    // // Regular expression to find URLs in the message
+    // const urlRegex = /(https?:\/\/[^\s]+)/g;
+    // const urls = message.match(urlRegex); // Finds all URLs in the message
 
-    if (!urls) {
-        // If no URLs are found, return the original message
+    if (!urls.length) {
+        // If no URLs are found, returns the original message
         return res.json({ message });
     }
 
@@ -75,12 +91,12 @@ app.post('/shortenURL', async (req, res) => {
     
     try {
         // Shortening logic
-        const shortenPromises = urls.map(url => shortenLink(url)); // Create an array of promises to shorten each URL
-        const shortenedUrls = await Promise.all(shortenPromises); // Wait for all promises to resolve
+        const shortenPromises = urls.map(url => shortenLink(url)); // Creates an array of promises to shorten each URL
+        const shortenedUrls = await Promise.all(shortenPromises); // Waits for all promises to resolve
 
         shortenedUrls.forEach((shortenedUrl, index) => {
             modifiedMessage = modifiedMessage.replace(urls[index], shortenedUrl.link);
-        }); // Replace original URLs with shortened URLs in the message
+        }); // Replaces original URLs with shortened URLs in the message
 
         // Logs formatted message
         logger.info("Formatted message", { message: modifiedMessage });
